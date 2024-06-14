602d169bf2502d28a661059c82f4e278f991fe31
Michael Reuter <mareuter@lsst.org>
2024-06-14 16:55:36 -0700

    Use prefix in production build.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  output: "standalone",
+  output: 'standalone',
+  basePath: isProd ? '/obsenv-management' : undefined,
 }
 
 export default nextConfig
