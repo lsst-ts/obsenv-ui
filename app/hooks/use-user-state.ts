f303fe0443056b6d7da718821a30a8ab3b1c0374
Michael Reuter <mareuter@lsst.org>
2024-10-01 05:19:10 -0700

    Streamline auth dance.

---
+++ app/hooks/use-user-state.ts
@@ -0,0 +1,54 @@
+import useSWR, { Fetcher } from 'swr'
+import { UserData, UserState } from '@/app/lib/definitions'
+
+const fetcher: Fetcher<UserData, string> = (api: string) =>
+  fetch(api).then((res) => res.json())
+
+/*
+ * A React hook for getting data from the `/auth/user-info` endpoint and
+ * establishing in general whether the user is logged in.
+ */
+function useUserState() {
+  var userState: UserState = {
+    loggedIn: false,
+    authorized: false,
+    data: {} as UserData,
+  }
+  let env = process.env.NODE_ENV
+  var userInfoApi: string
+  if (env === 'development') {
+    userInfoApi = '/api/dev/user-info'
+  } else if (env === 'production') {
+    userInfoApi = '/auth/api/v1/user-info'
+  } else {
+    throw Error(`Cannot get user state for ${env} environment!`)
+  }
+
+  const { data, error, isLoading } = useSWR<UserData, Error>(
+    userInfoApi,
+    fetcher,
+  )
+
+  if (error) {
+    throw Error(error.message)
+  }
+
+  const isLoggedIn = !error && data && data.hasOwnProperty('username')
+
+  if (isLoggedIn) {
+    userState.loggedIn = isLoggedIn
+    userState.data = data
+
+    const auth_group = process.env.NEXT_PUBLIC_AUTH_GROUP
+    userState.authorized =
+      data.groups.find(({ name }) => name === auth_group) === undefined
+        ? false
+        : true
+  }
+  return {
+    userState: userState,
+    isLoading,
+  }
+}
+
+export default useUserState
--- app/ui/dashboard/banner.tsx
+++ app/ui/dashboard/banner.tsx
@@ -1,38 +1,22 @@
 'use client'
 
-import { useContext, useState } from 'react'
-import { getLogin, getUserState } from '@/app/lib/actions'
-import { UserState } from '@/app/lib/definitions'
-import UserMenu from '@/app/ui/dashboard/user-menu'
+import { useContext } from 'react'
+import useUserState from '@/app/hooks/use-user-state'
 import { AuthContext } from '@/app/lib/auth-context'
 
 const Banner = () => {
-  const [userState, setUserState] = useState({} as UserState)
+  const { userState } = useUserState()
   const { setAuthed } = useContext(AuthContext)
-
-  const doLogin = async (event: any) => {
-    event.preventDefault()
-    await getLogin()
-    let s: UserState = await getUserState()
-    setUserState((userState) => ({ ...userState, ...s }))
-    setAuthed(s.authorized)
-  }
+  setAuthed(userState.authorized)
 
   return (
     <header className="fixed top-0 z-10 flex grid h-24 w-screen grid-cols-3 flex-row backdrop-blur-lg md:grid-cols-5">
       <h1 className="col-span-2 py-3 pl-4 text-lg md:col-span-3 md:text-xl">
         Rubin Observatory Environment Management
       </h1>
-      {!userState.loggedIn ? (
-        <button
-          onClick={doLogin}
-          className="col-span-1 col-end-4 hover:bg-gray-600 focus:outline-none active:bg-gray-700 md:col-end-6 md:text-lg"
-        >
-          Login
-        </button>
-      ) : (
-        <UserMenu userState={userState} />
-      )}
+      <h2 className="col-span-1 col-end-4 py-3 pr-4 md:col-end-6 md:text-lg">
+        {!userState.data ? 'Welcome!' : userState.data.username}
+      </h2>
     </header>
   )
 }
