01ab06a0d8844aa5db93f1d297c8915fbebaa9cd
Michael Reuter <mareuter@lsst.org>
2024-11-14 14:09:31 -0700

    Fix light/dark mode colors.

--- app/ui/change/form.tsx
+++ app/ui/change/form.tsx
@@ -48,7 +48,7 @@ const Form = () => {
         <input id="isTag" name="isTag" type="checkbox" />
       </div>
       <div className="col-span-1 py-3">
-        <button className="rounded bg-gray-800 p-2 shadow-md shadow-gray-400">
+        <button className="rounded bg-gray-300 p-2 shadow-md shadow-gray-600 dark:bg-gray-800 dark:shadow-gray-400">
           Submit
         </button>
       </div>
--- app/ui/dashboard/box-card.tsx
+++ app/ui/dashboard/box-card.tsx
@@ -21,7 +21,7 @@ const BoxCard = ({
   }
 
   return (
-    <div className="h-350 w-375 m-2 max-w-sm overflow-hidden rounded bg-gray-900 shadow-md shadow-gray-400">
+    <div className="h-350 w-375 m-2 max-w-sm overflow-hidden rounded bg-gray-400 shadow-md shadow-gray-600 dark:bg-gray-900 dark:shadow-gray-400">
       <div className="text-m item-center py-3 text-center font-bold">
         {name}
       </div>
@@ -39,13 +39,16 @@ const BoxCard = ({
             className={clsx(
               'rounded',
               'shadow-md',
-              'shadow-gray-400',
-              'bg-gray-800',
+              'shadow-gray-600',
+              'dark:shadow-gray-400',
+              'bg-gray-300',
+              'dark:bg-gray-800',
               'p-2',
               {
-                'hover:bg-gray-600 focus:outline-none focus:ring focus:ring-gray-300 active:bg-gray-700':
+                'hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-800 active:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-300 dark:active:bg-gray-700':
                   isAuthed,
-                'fg-gray-200 text-gray-700': !isAuthed,
+                'fg-gray-800 dark:fg-gray-200 text-gray-200 dark:text-gray-700':
+                  !isAuthed,
               },
             )}
           >
