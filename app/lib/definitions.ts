8f75ae1d0a955864e147b0c9b890542642e076d8
Michael Reuter <mareuter@lsst.org>
2025-06-13 23:00:30 -0700

    Fix.

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -42,5 +42,4 @@ export type PackageUpdate = {
   userid: string
 }
 
-// export type SearchParams = Record<string, string | string[] | undefined>
 export type SearchParams = { username: string; uid: string }
