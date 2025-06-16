ac6c1b4341615ae559f2acb70218c21a549d9a49
Michael Reuter <mareuter@lsst.org>
2025-06-16 09:14:29 -0700

    Center package change form on viewport.

--- app/ui/change/form.tsx
+++ app/ui/change/form.tsx
@@ -34,35 +34,37 @@ const Form = () => {
   }
 
   return (
-    <form action={dispatch} className="ml-3 mt-3 flex grid grid-cols-4">
-      <div className="col-span-4 py-3">
-        <label>Package Name: {packageName}</label>
-      </div>
-      <div className="col-span-4 py-3">
-        <label className="pr-2">
-          Version
-          <input
-            id="version"
-            name="version"
-            type="text"
-            defaultValue=""
-            className="text-black"
-            required
-          />
-        </label>
-      </div>
-      <div className="col-span-4 py-3">
-        <label className="pr-2">
-          Is Tag?
-          <input id="isTag" name="isTag" type="checkbox" />
-        </label>
-      </div>
-      <div className="col-span-1 py-3">
-        <button className="rounded bg-gray-300 p-2 shadow-md shadow-gray-600 hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-800 active:bg-gray-500 dark:bg-gray-800 dark:shadow-gray-400 dark:hover:bg-gray-600 dark:focus:ring-gray-300 dark:active:bg-gray-700">
-          Submit
-        </button>
-      </div>
-    </form>
+    <div className="flex h-screen items-center justify-center ">
+      <form action={dispatch} className="ml-3 mt-3 flex grid grid-cols-4">
+        <div className="col-span-4 py-3">
+          <label>Package Name: {packageName}</label>
+        </div>
+        <div className="col-span-4 py-3">
+          <label className="pr-2">
+            Version
+            <input
+              id="version"
+              name="version"
+              type="text"
+              defaultValue=""
+              className="text-black"
+              required
+            />
+          </label>
+        </div>
+        <div className="col-span-4 py-3">
+          <label className="pr-2">
+            Is Tag?
+            <input id="isTag" name="isTag" type="checkbox" />
+          </label>
+        </div>
+        <div className="col-span-1 py-3">
+          <button className="rounded bg-gray-300 p-2 shadow-md shadow-gray-600 hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-800 active:bg-gray-500 dark:bg-gray-800 dark:shadow-gray-400 dark:hover:bg-gray-600 dark:focus:ring-gray-300 dark:active:bg-gray-700">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
